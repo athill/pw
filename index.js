0da6f4b64089cec36af0938d4077c5def2e3d1c5
Andy Hill <andy@andyhill.us>
2018-08-10 19:36:53 -0400

    various refactors

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
 
+// constants
+//// type choices
 const UPPERCASE = 'uppercase',
 	LOWERCASE= 'lowercase',
 	NUMBERS = 'numbers',
 	SYMBOLS = 'symbols';
-
 const VALID_TYPE_CHOICES = [ UPPERCASE, LOWERCASE, NUMBERS, SYMBOLS ];
 const DEFAULT_TYPE_CHOICES = [UPPERCASE, LOWERCASE, NUMBERS];
 
+// length
+const DEFAULT_LENGTH = 20;
+
+// config path
+const CONFIG_PATH = './.config.json';
+
+// validate arguments at some point
+
+// help display
 if (process.argv.includes('--help')) {
 	console.log(`
 pw - a super simple password generating wizard.
@@ -24,12 +35,35 @@ Options:
 	process.exit(0);
 }
 
-process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
 
-});
+//config
+let config = {
+	types: DEFAULT_TYPE_CHOICES,
+	length: DEFAULT_LENGTH
+};
 
-inquirer.prompt([
+if (fs.existsSync(CONFIG_PATH)) {
+	try {
+		const obj = JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf8'));
+		console.log('um', obj);
+	} catch (e) {
+		if (e instanceof SyntaxError) {
+			console.log('Invalid syntax');
+		} else {
+			throw e;
+		}
+	}
+	
+} else {
+	console.log('No config file found. Run `pw --config to update prefernces`.');	
+}
+
+if (process.argv.includes('--config')) {
+	
+	process.exit(0);
+}
+
+const promt = async () => inquirer.prompt([
 	{
 		type: 'checkbox',
 		message: `Character types (default is ${DEFAULT_TYPE_CHOICES.join(', ')})`,
@@ -41,11 +75,14 @@ inquirer.prompt([
 		type: 'input',
 		message: 'Length',
 		name: 'length',
-		default: 20,
+		default: DEFAULT_LENGTH,
 		validate: input => /^\d*$/.test(input)
 	}
 
-]).then(answer => {
+]);
+
+const prompt = promt();
+prompt.then(answer => {
 	const charsTypes = {
 		[UPPERCASE]: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
 		[LOWERCASE]: 'abcdefghijklmnopqrstuvwxyz'.split(''),
@@ -58,7 +95,7 @@ inquirer.prompt([
 	});
 	let pw = '';
 	for (let i = 0; i < answer.length; i++) {
-		const index = Math.floor(Math.random() * Math.floor(valid.length));
+		const index = Math.floor(Math.random() * valid.length);
 		pw += valid[index];
 	}
 	console.log(pw);
